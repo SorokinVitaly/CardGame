e79afe01738645936f9b47952dc32502c2a6b26e
Vitaly Sorokin <sorokinvitaly@gmail.com>
2026-07-13 21:44:37 +0300

    Fix UI bug

--- app/src/main/java/com/example/cardgame/UiTools.kt
+++ app/src/main/java/com/example/cardgame/UiTools.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -54,8 +53,6 @@ fun Player(
             style = MaterialTheme.typography.titleMedium,
             color = Color.White
         )
-
-        Spacer(Modifier.height(8.dp))
 
         Box {
             playerData.cards.forEachIndexed { index, card ->
